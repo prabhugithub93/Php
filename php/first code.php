25d4e6225bce567e83da22f170547466a036564d
Prabhu <prabhumaurya9328@gmail.com>
2024-04-15 20:40:37 +0530

    added php

---
+++ php/first code.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>php</title>
+</head>
+<body>
+
+	<?php 
+echo " <h1> <b> hello  yahoo baba </b> </h1> ";
+
+	 ?>
+
+</body>
+</html>
